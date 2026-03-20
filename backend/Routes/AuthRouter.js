c3142aaf2094ffc3d640111295ebd990808778e6
Atul Kumar <atulald97@gmail.com>
2026-03-20 15:57:50 +0530

    add middleware file

--- backend/Routes/AuthRouter.js
+++ backend/Routes/AuthRouter.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const { signupValidation, loginValidation } = require('../middleware/Authvalidation');
-const { signup, login } = require('../controllers/AuthController');
+const { signupValidation, loginValidation } = require('../middleware/AuthValidation.js');
+const { signup, login } = require('../controllers/AuthController.js');
 const router = express.Router();   
 
 router.post('/login', loginValidation,login);
